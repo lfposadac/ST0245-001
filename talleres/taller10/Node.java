9676552f3fd8af7ddd004ac413e6e3f5d3323aef
lfposadac <lfposadac@eafit.edu.co>
2020-11-12 14:51:40 -0500

    Add files via upload

---
+++ talleres/taller10/Node.java
@@ -0,0 +1,14 @@
+
+/**
+ * @author (Luis Fernando Posada Cano) 
+ * @version (25/10/2020)
+ */
+public class Node {
+ public Node left;
+ public Node right;
+ public int data;
+ public Node(int d){
+	 
+	data = d;
+ }
+}
