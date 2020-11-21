8a13ffd2b71f5d2754861469fe43b325a7505110
lfposadac <lfposadac@eafit.edu.co>
2020-11-20 21:02:33 -0500

    Add files via upload

--- proyecto/codigo/main.java
+++ proyecto/codigo/main.java
@@ -11,14 +11,14 @@ import javafx.util.Pair;
 public class main
 {
     public static void main(String[] args) throws Exception{
-        BufferedReaderClass br = new BufferedReaderClass("prueba.csv");
+        BufferedReaderClass br = new BufferedReaderClass("4_train_balanced_135000.csv");
         BuildTree t = new BuildTree();
         long start = System.currentTimeMillis();
         Node y = t.decisionTree(br.matrix);
         long finalT = System.currentTimeMillis();
         System.out.println("It's takes " + (finalT -start));
         
-        BufferedReaderClass br2 = new BufferedReaderClass("test.csv");
+        BufferedReaderClass br2 = new BufferedReaderClass("4_test_balanced_135000.csv");
         double[] in = Test.information(br2.matrix, y);
         System.out.println("Precision: " + in[0]);
         System.out.println("Sensibilidad: " + in[1]);
